a7803c8e0a51f323a59bd154bfc694dc911f9b35
sundarasekarg <sundarasekarg75@gmail.com>
2026-07-17 13:57:34 -0400

    Add unit tests for emotion detection

---
+++ test_emotion_detection.py
@@ -0,0 +1,29 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+
+class TestEmotionDetection(unittest.TestCase):
+
+    def test_emotion_detector(self):
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result["dominant_emotion"], "joy")
+
+    def test_emotion_detector_anger(self):
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result["dominant_emotion"], "anger")
+
+    def test_emotion_detector_disgust(self):
+        result = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result["dominant_emotion"], "disgust")
+
+    def test_emotion_detector_sadness(self):
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result["dominant_emotion"], "sadness")
+
+    def test_emotion_detector_fear(self):
+        result = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result["dominant_emotion"], "fear")
+
+
+if __name__ == "__main__":
+    unittest.main()
